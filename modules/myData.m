ef9c6131f7bc71924e50ad56636f8f98790fca02
grj <321312739@qq.com>
2023-04-19 01:05:05 +0800

    添加忽略文件； 更改modules/myData.m中的默认值

--- modules/myData.m
+++ modules/myData.m
@@ -2,9 +2,9 @@
 classdef myData < handle
     %定义默认量
     properties
-        alpha = 0;                 %水印嵌入强度
+        alpha = 0.1;               %水印嵌入强度
         insetType = '低频';        %嵌入频段
-        dwtName = 'db2';           %小波基名称
+        dwtName = 'haar';          %小波基名称
         extractType = 1;           %水印提取模式，正常提取1，攻击后提取2，jpg压缩后提取3
     end
 end
